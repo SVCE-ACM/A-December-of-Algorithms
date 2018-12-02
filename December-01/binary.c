85159ac3f2833dc9af8c459e7a2591e6557b5cdf
Sushmakarunakaran <45537089+Sushmakarunakaran@users.noreply.github.com>
2018-12-02 23:00:01 +0530

    Add files via upload

---
+++ December-01/binary.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a[25],first,last,mid,search,n,i,c,s;
+	do
+		{
+		
+	printf("enter the no of elements ");
+	scanf("%d",&n);
+	printf("enter sorted elements ");
+	
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("enter the element to be searched");
+	scanf("%d",&search);
+	first=0;
+	last=n-1;
+	while(first<=last)
+	{
+		mid=(first+last)/2;
+		if(search==a[mid])
+		{
+		  printf("the element is found at %d ",mid+1);
+		  c++;
+		  break;
+		}
+	    if(search<a[mid])
+		{
+			last=mid-1;
+		}
+		if(search>a[mid])
+		{
+			first=mid+1;
+		}
+		
+	}
+	if(first>last)
+	{
+		printf("the element is not found in the given array");
+	}
+	printf("enter 1 to continue and 0 to end ");
+	scanf("%d",&s);
+	
+}while(s>0);
+}
