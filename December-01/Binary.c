b35d53c0e412af89f7ecbba21c4c926960bc95e6
Thejesh-404 <45538592+Thejesh-404@users.noreply.github.com>
2018-12-05 10:14:59 +0530

    Update Binary.c

--- December-01/Binary.c
+++ December-01/Binary.c
@@ -25,7 +25,7 @@ void main()
 	    mid=(low+high)/2;	    
 	    if(s==a[mid])
 	    {
-	    printf("\n  spot on  found @ pos=%d",mid+1);
+	    printf("\n  spot on @ pos=%d",mid+1);
 	    c++;
 	    break;
 	}
