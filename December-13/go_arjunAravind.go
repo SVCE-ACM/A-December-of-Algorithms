b0d5c66ce220d265c74795f73bb9308d42b764da
arjun1098 <arjun.aravind1998@gmail.com>
2018-12-06 00:14:55 +0530

    Start Algo13

---
+++ December-13/go_arjunAravind.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"../December12"
+)
+
+/**
+  * We need to print all possible permutations of a given string.
+ */
+
+func main() {
+	list := linkedlist.NewList() 
+
+	list.Add("Arjun")
+	list.Print()
+}
