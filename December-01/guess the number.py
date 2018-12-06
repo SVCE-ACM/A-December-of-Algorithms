2c3b3ceb3ef9759faf82ad0d0f50f63e8f0072be
shriaas2898 <aasthashrivastava410@gmail.com>
2018-12-06 23:55:34 +0530

    Guess the number program

---
+++ December-01/guess the number.py
@@ -0,0 +1,11 @@
+#Program for guessing the number
+
+print("--------------------------Welcome to guess the number------------------")
+
+number = int(input("Player A enter a number between 0 to 100"))
+
+print("Player B you have 7 tries to guess the correct number")
+
+for guess_no in range(7):
+    guess = int(input("Enter your guessed number"))
+    if(guess)
