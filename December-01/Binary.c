1ed49f8ed5061bea0578779c3453ca390716844f
Thejesh-404 <45538592+Thejesh-404@users.noreply.github.com>
2018-12-06 21:00:44 +0530

    Update Binary.c

--- December-01/Binary.c
+++ December-01/Binary.c
@@ -2,51 +2,21 @@
 #include<stdio.h>
 void main()
 {
-	int a[20],n,s,high,low,mid,i,c=0,d,e;
-	do
-	{
-	
-	printf("Enter the number of elements: ");
-	scanf("%d",&n);
-	printf("\n\t\t\t\tEnter elements in order");
-	for(i=0;i<n;i++)
-	{
-		printf("\n Enter element %d:",i+1);
-		scanf("%d",&a[i]);
-	}		
-	do
-	{	
-	printf("\nEnter the element to be searched:");
-	scanf("%d",&s);
-	low=0;
-	high=n-1;	
-	while(high>=low)
-	{
-	    mid=(low+high)/2;	    
-	    if(s==a[mid])
-	    {
-	    printf("\nspot on @ pos=%d",mid+1);
-	    c++;
-	    break;
-	}
-	else if(s>a[mid])
-	{
-    low=mid+1;
-	printf("\nyou are too low");
-	}
-	else
-	{
-	high=mid-1;
+int s,g;	
+printf("Say my chosen number is :");
+scanf("%d",&s);
+while(1)
+{
+printf("\n\nGuess:");
+scanf("%d",&g);
+if(s==g)
+{
+	printf("\nSpot ON !!!");
+	break;
+}
+else if(s>g)
+	printf("\nyou are too low.");
+else
 	printf("\nyou are too high");	
 }
-	}		
-if(c==0)
-printf("\n The element is not found : sorry guys");
-	printf("\n\n\t\t\tDo you want to continue your search process (yes==1 or No == 0)");
-	scanf("%d",&d);			
-}while(d==1);
-printf("\n\n\t\t\t\tDo you want to continue the whole process (yes==1 or NO == 0)");	
-scanf("%d",&e);
-}while(e==1);
 }
-									      
