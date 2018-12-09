ac10b77c1162faffe44f85d655f669a8efb07d0a
raf1800 <32974129+raf1800@users.noreply.github.com>
2018-12-09 11:29:33 +0530

    Dec 7

---
+++ December-08/python_raf1800.py
@@ -0,0 +1,19 @@
+def SingularPlural(s1,s2):
+    if s2.isnumeric():
+        if(int(s2)==-1 or int(s2)==1):
+            print(s1)
+        else:
+            print(s1 + "s")
+    else:
+        if s1.star
+        
+        
+        
+
+def main():
+    s1 = input("Enter a word: ")
+    s2 = input("Enter a string or another number: ")
+    SingularPlural(s1,s2)
+
+if __name__ == "__main__":
+    main()
