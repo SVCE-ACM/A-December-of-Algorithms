dbbd0ce0596e4b0551178d5561a8e503b749b4fa
imhphari <imhphari@gmail.com>
2018-12-26 00:34:47 +0530

    Create py_imhphari.py

---
+++ December-15/py_imhphari.py
@@ -0,0 +1,9 @@
+def pascal_triangle(n):
+   trow = [1]
+   y = [0]
+   for x in range(max(n,0)):
+      print(trow)
+      trow=[l+r for l,r in zip(trow+y, y+trow)]
+   return n>=1
+n=input("enter n:")
+pascal_triangle(int(n)) 
