399bbd4599ff77b3606588e6ddff204055034459
imhphari <imhphari@gmail.com>
2018-12-26 00:13:01 +0530

    Create py_imhphari.py

---
+++ December-14/py_imhphari.py
@@ -0,0 +1,18 @@
+def encrypt(text,s): 
+    result = "" 
+  
+    for i in range(len(text)): 
+        char = text[i] 
+  
+        if (char.isupper()): 
+            result+=chr((ord(char)+s-65)%26+65) 
+        else: 
+            result+=chr((ord(char)+s-97)%26+97) 
+  
+    return result 
+  
+text = input("Enter string:")
+s = 3
+print ("Text  : " + text) 
+print ("Shift : " + str(s)) 
+print ("Cipher: " + encrypt(text,s) )
