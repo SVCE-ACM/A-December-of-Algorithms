d55dba5127d7f3ce9d9c0204a3afeceea1498d6f
imhphari <imhphari@gmail.com>
2018-12-26 18:33:58 +0530

    Update py_imhphari.py

--- December-15/py_imhphari.py
+++ December-15/py_imhphari.py
@@ -1,9 +1,15 @@
-def pascal_triangle(n):
-   trow = [1]
-   y = [0]
-   for x in range(max(n,0)):
-      print(trow)
-      trow=[l+r for l,r in zip(trow+y, y+trow)]
-   return n>=1
-n=input("enter n:")
-pascal_triangle(int(n)) 
+n=int(input("Enter number of rows: "))
+a=[]
+for i in range(n):
+    a.append([])
+    a[i].append(1)
+    """print(a[i])"""
+    for j in range(1,i):
+        a[i].append(a[i-1][j-1]+a[i-1][j])
+    if(n!=0):
+        a[i].append(1)
+for i in range(n):
+    print("   "*(n-i),end=" ",sep=" ")
+    for j in range(0,i+1):
+        print('{0:6}'.format(a[i][j]),end=" ",sep=" ")
+    print()
