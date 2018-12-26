0b0f840af82954f3a115d9be086e1a6afa94f794
Raahul kalyaan <rahulr4469@gmail.com>
2018-12-26 18:52:11 +0530

    Update python_exampleUser.py

--- December-24/python_exampleUser.py
+++ December-24/python_exampleUser.py
@@ -6,3 +6,17 @@ This file doesn't contain any code.
 It's just here to give an example of the file naming scheme.
 Cheers!
 """
+
+def revstr(string,str,len):
+    if(len!=0):
+        str+=string[len]
+        revstr(string,str,len-1)
+    else:
+        print(str)
+def main():
+    string=input("ENTER STRING")
+    str=" "
+    length1=len(string)
+    revstr(string,str,length1-1)
+if __name__ == '__main__':
+    main()
