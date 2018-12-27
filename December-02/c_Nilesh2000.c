4eae94a53f397136459efbd7b26eb0fd2bf30110
Nilesh2000 <nileshlund@gmail.com>
2018-12-27 20:21:38 +0530

    Done with the input.

---
+++ December-02/c_Nilesh2000.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+int main(void)
+{
+  int i, Side1[3], Side2[3], Angle1[3], Angle2[3];
+  printf("\nInput:\n");
+  printf("side1 = ");
+   for(i=0;i<3;i++)
+    {
+      scanf("%d", &Side1[i]);
+    }
+  printf("angle1 = ");
+   for(i=0;i<3;i++)
+    {
+      scanf("%d", &Angle1[i]);
+    }
+  printf("angle1 = ");
+   for(i=0;i<3;i++)
+    {
+      scanf("%d", &Side2[i]);
+    }
+  printf("angle1 = ");
+   for(i=0;i<3;i++)
+    {
+      scanf("%d", &Angle2[i]);
+    }
+}
