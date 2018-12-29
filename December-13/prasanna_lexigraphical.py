66528a60a9dba1d382fc76660933d13fa178849d
prasanna77cr7 <42204974+prasanna77cr7@users.noreply.github.com>
2018-12-29 20:48:01 +0530

    Add files via upload

---
+++ December-13/prasanna_lexigraphical.py
@@ -0,0 +1,16 @@
+rom itertools import permutations 
+a=[] 
+def allPermutations(str): 
+     permList = permutations(str) 
+     for perm in list(permList): 
+         a.append(''.join(perm)) 
+          
+x=input("Enter the word")
+allPermutations(x)
+z=1   
+a.sort()
+for i in a:
+  if i==x:
+    print(z)
+  else:
+    z=z+1  
