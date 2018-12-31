5c2a7dc686804ad8c925eab9c91853843f7a5fc1
Ajay Krishnan <42097993+ajaykrishnan23@users.noreply.github.com>
2018-12-31 22:24:13 +0530

    Add files via upload

---
+++ December-21/py_ajaykrishnan23.py
@@ -0,0 +1,33 @@
+import csv
+def convert():
+	f = open('Dec21-Exchange_Rates.csv',"r")
+	reader = csv.reader(f)
+	dict = {}
+	for row in reader:
+		dict[row[0]] = row[1]
+	fromc = input('Enter country')
+	toc = input('Enter country to which to be converted')
+	amt = float(input('enter the amount to be converted'))
+	print(dict[fromc])
+	amt_new = amt/float(dict[fromc])
+	print(dict[toc])
+	amt_new = amt_new*float(dict[toc])
+	print(str(amt)+'('+'fromc) = '+ str(amt_new) + '(Toc)')
+	
+def showall4country():
+	f = open('Dec21-Exchange_Rates.csv',"r")
+	reader = csv.reader(f)
+	dict = {}
+	for row in reader:
+		dict[row[0]] = row[1]
+	fromc = input('Enter country whose value in each other country is to be found')
+	del dict['LOCATION']
+	for entry in dict:
+		print(entry +' : '+ str(float(dict[entry])/float(dict[fromc])))
+
+ch = int(input('1->Currency converter\n2->Value of that country currency in the other countries'))
+if ch==1:
+	convert()
+else:
+	showall4country()
+		
