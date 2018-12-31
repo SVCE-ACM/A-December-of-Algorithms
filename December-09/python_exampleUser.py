191630452ae7979492d5de227df22a9dbd3f0d97
surudhi <surudhi.asokraj@gmail.com>
2018-12-31 22:56:51 +0530

    Update python_exampleUser.py

--- December-09/python_exampleUser.py
+++ December-09/python_exampleUser.py
@@ -1,8 +1,7 @@
-#!python3
-
-"""
-Hi there.
-This file doesn't contain any code.
-It's just here to give an example of the file naming scheme.
-Cheers!
-"""
+import re
+str = input('Enter the URL: ')
+p = re.search('^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$', str) 
+if p:
+    print('True')
+else:
+    print('False')
