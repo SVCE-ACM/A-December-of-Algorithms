33e17e1dd9e4b5b12cea5b771434f432e9ad02a8
surudhi <surudhi.asokraj@gmail.com>
2018-12-31 22:55:28 +0530

    Update python_exampleUser.py

--- December-07/python_exampleUser.py
+++ December-07/python_exampleUser.py
@@ -1,8 +1,20 @@
-#!python3
-
-"""
-Hi there.
-This file doesn't contain any code.
-It's just here to give an example of the file naming scheme.
-Cheers!
-"""
+def round(n):
+    i=n-int(n)
+    if i:
+        if i>0.5:
+            n=float(int(n)+1)
+            return n
+        else:
+            n=float(int(n)-1)
+            return n
+    else:
+        return n
+a=float(input('Enter the first number: '))
+b=float(input('Enter the second number: '))
+tol=float(input('Enter the tolerance level: '))
+c=round(a)
+d=round(b)
+if (c==d) and abs(a-b)<tol:
+    print(True)
+else:
+    print(False)
