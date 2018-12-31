441e108043a8e2f38d789883e16aefdf468c52c2
surudhi <surudhi.asokraj@gmail.com>
2018-12-31 22:49:15 +0530

    Update python_exampleUser.py

--- December-01/python_exampleUser.py
+++ December-01/python_exampleUser.py
@@ -1,8 +1,23 @@
-#!python3
+import random
+x=random.randint(1,100)
+flag=1
+u=1
+l=100
+while flag:
+    g=int(input('Enter your guess: '))
+    if x==g:
+        flag=0
+        print('Your guess is right,',x)
+    else:
+        if x>g:
+            print('Your guess is lesser')
+            u=g
+            print('Guess a number between',u,' and ',l)    
+        else:
+            print('Your guess is more')
+            l=g
+            print('Guess a number between',u,' and ',l)
+            
+         
 
-"""
-Hi there.
-This file doesn't contain any code.
-It's just here to give an example of the file naming scheme.
-Cheers!
-"""
+
