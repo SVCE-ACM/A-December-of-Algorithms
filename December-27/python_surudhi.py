1a3501499b34f8bc20bc0ab73fa38a0093acaeaf
surudhi <surudhi.asokraj@gmail.com>
2018-12-31 23:16:43 +0530

    Update and rename python_exampleUser.py to python_surudhi.py

---
+++ December-27/python_surudhi.py
@@ -0,0 +1,7 @@
+def VowelSquare(a):
+    for i in range(len(a) - 1):
+        for l in range(len(a[i]) - 1):
+            if a[i][l] in 'aeiou' and a[i][l+1] in 'aeiou' and a[i+1][l] in 'aeiou' and a[i+1][l+1] in 'aeiou':
+                return str(i) + '-' + str(l)
+    return 'not found'
+print(VowelSquare(input()))
