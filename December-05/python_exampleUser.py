70492db8f67c9b2def7b503a7c976c3a0b70fbd6
surudhi <surudhi.asokraj@gmail.com>
2018-12-31 22:53:45 +0530

    Update python_exampleUser.py

--- December-05/python_exampleUser.py
+++ December-05/python_exampleUser.py
@@ -1,8 +1,12 @@
-#!python3
+def moveDisk(fp,tp):
+    print(fp,"=>",tp)
+def moveTower(height,fromPole, toPole, withPole):
+    if height >= 1:
+        moveTower(height-1,fromPole,withPole,toPole)
+        moveDisk(fromPole,toPole)
+        moveTower(height-1,withPole,toPole,fromPole)
+def Hanoi(n):
+    moveTower(n,"left","right","middle")   
+height=int(input('Enter the number of disks: '));
+Hanoi(height)
 
-"""
-Hi there.
-This file doesn't contain any code.
-It's just here to give an example of the file naming scheme.
-Cheers!
-"""
