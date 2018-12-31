be776c585634c18501d0edcbf7732784ea662fd0
surudhi <surudhi.asokraj@gmail.com>
2018-12-31 22:52:02 +0530

    Update python_exampleUser.py

--- December-03/python_exampleUser.py
+++ December-03/python_exampleUser.py
@@ -1,8 +1,24 @@
-#!python3
-
-"""
-Hi there.
-This file doesn't contain any code.
-It's just here to give an example of the file naming scheme.
-Cheers!
-"""
+def isLucky(n):
+    a=n
+    i=j=0
+    while n>0:
+        n/=10
+        i=1
+    sum1=sum2=0
+    j=i
+    while j>i/2: 
+        sum1+=a%10
+        a=a/10
+        j=j-1
+    while  j>0:
+        sum2+=a%10
+        a=a/10
+        j=j-1    
+    if sum1==sum2:
+        return True
+    else:
+        return False
+    
+n=int(input('Enter your ticket number: '))
+print(isLucky(n))
+    
