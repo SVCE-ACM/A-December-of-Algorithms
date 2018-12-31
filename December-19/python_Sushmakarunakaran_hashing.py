6e8d153fe0c42746493431557c80d2d7d4c7fdb6
Sushmakarunakaran <45537089+Sushmakarunakaran@users.noreply.github.com>
2018-12-31 20:22:45 +0530

    Add files via upload

---
+++ December-19/python_Sushmakarunakaran_hashing.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def hash(s):
+    l=[]
+    for i in s:
+        l.append(ord(i))
+    b=sum(l)
+    print(b/len(s))
+s=input("Enter the string:")
+hash(s)
+
+
+# In[ ]:
+
+
+
+
