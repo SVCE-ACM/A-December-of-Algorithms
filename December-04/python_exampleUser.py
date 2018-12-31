4d3f51de7477b0b242672063921268c9da8dddf1
surudhi <surudhi.asokraj@gmail.com>
2018-12-31 22:53:14 +0530

    Update python_exampleUser.py

--- December-04/python_exampleUser.py
+++ December-04/python_exampleUser.py
@@ -1,8 +1,24 @@
-#!python3
-
-"""
-Hi there.
-This file doesn't contain any code.
-It's just here to give an example of the file naming scheme.
-Cheers!
-"""
+def fib(n):
+    a=0
+    b=1
+    if n==1:
+        c=a
+    elif n==2:
+        c=b
+    while (n-2)>0:
+        c=a+b
+        a=b
+        b=c
+        n=n-1
+    return c
+n=int(input('Enter the value of N(Nth term): '))
+if n>3:
+    print(n,'th term of the Fibonacci Series is ',fib(n))
+elif n==3:
+    print(n,'rd term of the Fibonacci Series is ',fib(n))
+elif n==2:
+    print(n,'nd term of the Fibonacci Series is ',fib(n))
+elif n==1:
+    print(n,'th term of the Fibonacci Series is ',fib(n))
+else:
+    print('Invalid Input')
