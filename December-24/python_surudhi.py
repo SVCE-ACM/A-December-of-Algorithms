be9bfb73e9c513ab2f4f886885e66ed7ac922b50
surudhi <surudhi.asokraj@gmail.com>
2018-12-31 23:14:18 +0530

    Update and rename python_exampleUser.py to python_surudhi.py

---
+++ December-24/python_surudhi.py
@@ -0,0 +1,8 @@
+def reverse(s): 
+    if len(s) == 0: 
+        return s 
+    else: 
+        return reverse(s[1:]) + s[0] 
+s=input('Enter the string: ')
+rev=reverse(s)
+print('Reversed string:',rev)
