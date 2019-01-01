62aa012a43e619934776cb839a72f730e1d8ca9b
imhphari <imhphari@gmail.com>
2019-01-01 17:03:19 +0530

    Update py_imhphari.py

--- December-30/py_imhphari.py
+++ December-30/py_imhphari.py
@@ -1,3 +1,6 @@
 n=int(input("Enter the number of sides in the polygon:"))
-print("Number of diagonals: ",int(n*(n-3)/2))
+if(n<3):
+    print("Number of diagonals: 1")
+else:
+    print("Number of diagonals: ",int(n*(n-3)/2))
     
