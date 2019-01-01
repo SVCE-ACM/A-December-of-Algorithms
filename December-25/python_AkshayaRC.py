8a6bc58c2862d73f0fe42a54a39a5847e36ed749
AkshayaRC <43496984+AkshayaRC@users.noreply.github.com>
2019-01-01 21:17:41 +0530

    Christmas presents delivered-updated

--- December-25/python_AkshayaRC.py
+++ December-25/python_AkshayaRC.py
@@ -1,13 +1,4 @@
-print("*",end=" ")
-for i in range(10):
-    print(i,end=" ")
-print("",sep="\n")
-matrix=[["*" for i in range(0,10)] for j in range(0,10)]
-for i in range(0,10):
-    print(i,end=" ")
-    for j in range(0,10):
-      print(matrix[i][j],end=" ")
-    print("",sep="\n")  
+matrix=[["*" for i in range(0,10)] for j in range(0,10)] 
 print("\nSanta's location(row-column) (seperated by new line): ")
 sr=int(input())
 sc=int(input())
@@ -20,11 +11,21 @@ for i in range(sr,cr+1):
         if matrix[i][j]=="*":
             matrix[i][j]=" "
 matrix[sr][sc]="S"            
-cr=i
+i=cr
 for j in range(sc+1,cc+1):
     if matrix[i][j]=="*":
         matrix[i][j]=" "
+if sc>cc:
+    for j in range(sc-1,cc,-1):
+       if matrix[i][j]=="*":
+        matrix[i][j]=" "
 matrix[cr][cc]="K"
+
+if cr<sr:
+    j=sc
+    for i in range(sr,cr-1,-1):
+       if matrix[i][j]=="*":
+        matrix[i][j]=" " 
 print("A path between santa at (",sr,",",sc,") and child at (",cr,",",cc,"): ")
 print("*",end=" ")
 for i in range(10):
@@ -35,27 +36,3 @@ for i in range(0,10):
     for j in range(0,10):
       print(matrix[i][j],end=" ")
     print("",sep="\n")              
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
