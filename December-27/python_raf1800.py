37b9cb817242bdb5864b074e7abc51a7ef0928ca
raf1800 <32974129+raf1800@users.noreply.github.com>
2019-01-01 10:29:40 +0530

    27

--- December-27/python_raf1800.py
+++ December-27/python_raf1800.py
@@ -1,16 +1,16 @@
 def vowsq(StrArr):
-  vocal='aeiou'
+  vow='aeiou'
   for x in range (0,len(StrArr)-1):
     for y in range(0,len(StrArr[x])-1):
-      if(vocal.find(StrArr[x][y])>-1):
-        if(vocal.find(StrArr[x][y+1])>-1):
-          if(vocal.find(StrArr[x+1][y])>-1):
-            if(vocal.find(StrArr[x+1][y+1])>-1):
+      if(vow.find(StrArr[x][y])>-1):
+        if(vow.find(StrArr[x][y+1])>-1):
+          if(vow.find(StrArr[x+1][y])>-1):
+            if(vow.find(StrArr[x+1][y+1])>-1):
               return(str(x)+'-'+str(y))
   return("Unavailable")
 
 def main():
-    mat=[]
+    mat=list()
     n = int(input("Enter number of words: "))
     print("Enter words: ")
     for i in range(n):
