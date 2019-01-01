8a19193fd7923f924ec313c9d7ea6c8e8636111b
AkshayaRC <43496984+AkshayaRC@users.noreply.github.com>
2019-01-01 14:25:55 +0530

    Christmas present delivered

---
+++ December-25/python_AkshayaRC.py
@@ -0,0 +1,61 @@
+print("*",end=" ")
+for i in range(10):
+    print(i,end=" ")
+print("",sep="\n")
+matrix=[["*" for i in range(0,10)] for j in range(0,10)]
+for i in range(0,10):
+    print(i,end=" ")
+    for j in range(0,10):
+      print(matrix[i][j],end=" ")
+    print("",sep="\n")  
+print("\nSanta's location(row-column) (seperated by new line): ")
+sr=int(input())
+sc=int(input())
+print("Child's location(row-column) (seperated by new line): ")
+cr=int(input())
+cc=int(input())
+#print(sr,sc,cr,cc)
+j=sc
+for i in range(sr,cr+1):
+        if matrix[i][j]=="*":
+            matrix[i][j]=" "
+matrix[sr][sc]="S"            
+cr=i
+for j in range(sc+1,cc+1):
+    if matrix[i][j]=="*":
+        matrix[i][j]=" "
+matrix[cr][cc]="K"
+print("A path between santa at (",sr,",",sc,") and child at (",cr,",",cc,"): ")
+print("*",end=" ")
+for i in range(10):
+    print(i,end=" ")
+print("",sep="\n")
+for i in range(0,10):
+    print(i,end=" ")
+    for j in range(0,10):
+      print(matrix[i][j],end=" ")
+    print("",sep="\n")              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
