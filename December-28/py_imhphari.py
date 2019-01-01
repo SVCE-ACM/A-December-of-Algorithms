8a26dbdbadedbc597c2c67a825ef1ca6802c9283
imhphari <imhphari@gmail.com>
2019-01-01 17:22:18 +0530

    Update py_imhphari.py

--- December-28/py_imhphari.py
+++ December-28/py_imhphari.py
@@ -1,19 +1,39 @@
-import numpy as np
-import itertools
-
-matrix = np.array(
-         [[7 ,4,6,8], 
-        [1,7,4,6],
-         [9,1,7,4]])
-
-diags = [matrix[::-1,:].diagonal(i) for i in range(3,4)]
-diags.extend(matrix.diagonal(i) for i in range(3,-4,-1))
-
-for n in diags:
-    for i in range(len(n)):
-        for j in range(i + 1, len(n)):
-            if(n[i]==n[j]):
-                print("Identical")
-    
-
-#print ([n.tolist() for n in diags])
+m=int(input("enter no.of rows :"))
+n=int(input("enter no.of columns :"))
+flag=[]
+matrix=[[0 for j in range(n)] for i in range(m)]
+print("Enter matrix elements")
+for i in range(0,m):
+    for j in range(0,n):
+        matrix[i][j]=int(input())
+print("Given input matrix: ")
+for i in range(m):
+    for j in range(n):
+        print(matrix[i][j],end=" ")
+    print("",sep="\n")    
+for k in range(m-1,-1,-1):
+    i=k
+    j=0
+    while(i<=m-1):
+        if i+1<=m-1:
+           if matrix[i][j]==matrix[i+1][j+1]:
+               flag.append("1")
+           else:
+               flag.append("-1")
+        i+=1
+        j+=1
+for k in range(1,n):
+      i=0
+      j=k
+      while(j<=n-1):
+          if j+1<=n-1:
+            if matrix[i][j]==matrix[i+1][j+1]:
+               flag.append("1")
+            else:
+               flag.append("-1")
+          i+=1
+          j+=1
+if "-1" in flag:
+    print("\nDiagonals are not identical")
+else:
+    print("\nIdentical diagonals")
