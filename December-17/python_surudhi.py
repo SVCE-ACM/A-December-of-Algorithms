98d3500a751ac8788092b18b9a11ed46149947df
surudhi <surudhi.asokraj@gmail.com>
2019-01-05 14:50:22 +0530

    Update python_surudhi.py

--- December-17/python_surudhi.py
+++ December-17/python_surudhi.py
@@ -1,75 +1,54 @@
-from numpy import *
-def printsquare(m):
-    if m%2!=0:
-        k =[[0 for i in range(m)] for j in range(m)]
-        r=0;
-        c=int(m/2);
-    for i in range(1,m*m+1):
-        k[r][c]= i;
-        br = r+1;
-        bc = c+1;
-        r=(r+m-1)%m;
-        c=(c+1)%m;
-        if k[r][c]!=0:
-            r=br;
-            c=bc-1;
-    sq=[]
-    for row in k:
-        sq.append(row)
-    s=array([sq])
-    s1=reshape(s,(m,m))
-    print(s1)
-    print('\n')
-    s2=trans(s1,m)
-    s=array([s2])
-    s1=reshape(s,(m,m))
-    print(s1)
-    print('\n')
-    list.reverse(sq)
-    s=array([sq])
-    s1=reshape(s,(m,m))
-    print(s1)
-    print('\n')
-    s2=trans(s1,m)
-    s=array([s2])
-    s1=reshape(s,(m,m))
-    print(s1)
-    print('\n')
-    sq=[]
-    for row in k:
-        list.reverse(row)
-        sq.append(row)
-    s=array([sq])
-    s1=reshape(s,(m,m))
-    print(s1)
-    print('\n')
-    s2=trans(s1,m)
-    s=array([s2])
-    s1=reshape(s,(m,m))
-    print(s1)
-    print('\n')
-    list.reverse(sq)
-    s=array([sq])
-    s1=reshape(s,(m,m))
-    print(s1)
-    print('\n')
-    s2=trans(s1,m)
-    s=array([s2])
-    s1=reshape(s,(m,m))
-    print(s1)
-    print('\n')
-def trans(X,n):
-    result=[]
-    for i in range(n):
-        s=[]
-        for j in range(n):
-            s.append(0)
-        result.append(s)
-    for i in range(len(X)):
-        for j in range(len(X[0])):
-            result[j][i] = X[i][j]
-    return result
-n=int(input('Order: '))
-sum=n*(n*n+1)/2
-print('Magic Sum: ',sum)
-printsquare(n)
+import numpy as np
+
+
+def magic_odd(N, M):  # Odd
+    mat = np.zeros((N, N), dtype=int)
+    i, j = 0, N//2
+    num = 1
+    while num <= N**2:
+        mat[i, j] = num
+        num += 1
+        i = (i-1) % N
+        j = (j+1) % N
+        if mat[i, j]:
+            j = (j-1) % N
+            i = (i+2) % N
+    return mat
+
+
+n = int(input('Enter the Order: '))
+m = int(input('Enter the Magic Sum: '))
+
+
+def magic_four(N, M):  # Multiples of 4
+    mat = np.zeros((N, N), dtype=int)
+    i = 1
+    for j in range(N):
+        for k in range(N):
+            mat[j, k] = i
+            i += 1
+    j = N-1
+    for i in range(N):
+        if i > j:
+            break
+        mat[i, i], mat[j, j] = mat[j, j], mat[i, i]
+        j -= 1
+
+    for i in range(N//2):
+        mat[i, N-i-1], mat[N-i-1, i] = mat[N-i-1, i], mat[i, N-i-1]
+    return mat
+
+
+if n % 2 != 0:
+    matrix = magic_odd(n, m)
+else:
+    matrix = magic_four(n, m)
+
+print("All matrices")
+print(matrix)
+print("\n", np.transpose(matrix))
+for i in range(n):
+    matrix = np.flip(matrix, 1)
+    print("\n", matrix)
+    matrix = np.transpose(matrix)
+    print("\n", matrix)
