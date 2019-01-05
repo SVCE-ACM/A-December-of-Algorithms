13f016d486da57b8dae80bb200531bafe246e887
Nilesh2000 <nileshlund@gmail.com>
2019-01-05 19:22:15 +0530

    Solved the algorithm finally :)

--- December-25/c_Nilesh2000.c
+++ December-25/c_Nilesh2000.c
@@ -12,23 +12,23 @@ int main(void)
 	scanf("%d %d", &Santa.x, &Santa.y);
 	printf("Child's Location: ");
 	scanf("%d %d", &Child.x, &Child.y);
-	for(int i=0;i<3;i++)
+	for(int i=0;i<10;i++)
 	{
-		for(int j=0;j<3;j++)
-		{
-			if(i==Santa.x && j==Santa.y)
-			 Mat[i][j]='S';
-			else if(i==Child.x && j==Child.y)
-			 Mat[i][j]='K';
-			else if(i>=Santa.x+1 && i<Child.x+1)
-			 Mat[i][Santa.y]=' ';
-			else if(j>=Santa.y+1 && j<Child.y)
-			 Mat[Child.x][j]=' ';
-			else
-			 Mat[i][j]='*';
-			printf("%c ", Mat[i][j]);
-		}
 	 printf("\n");
-	}
+		for(int j=0;j<10;j++)
+		{
+			if(i==Santa.y && j==Santa.x)
+			 printf("S ");
+			else if(i==Child.y && j==Child.x)
+			 printf("K ");
+			else if((i==Santa.y && (j>Child.x && j<Santa.x)) || (i==Child.y && (j>Santa.x && j<Child.x)) && (Santa.y<Child.y || Santa.x< Child.x))
+			 printf("  ");
+			else if((j==Santa.x && (i>Santa.y && i<=Child.y)) || (j==Child.x && (i>Child.y && i<=Santa.y)))
+			 printf(" ");
+			else
+			 printf("* ");
+		}
+  }
+	printf("\n");
 	return 0;
 }
