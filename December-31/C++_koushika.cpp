2d0ffe92096c10230b2bef496d4003755b4aee99
KoushikaK7 <35607067+KoushikaK7@users.noreply.github.com>
2019-01-06 20:13:47 +0530

    Initial Commit

---
+++ December-31/C++_koushika.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+  
+void func(int n) 
+{ 
+    int i, j = 1, k = 1; 
+  
+    // For each iteration increase j by 1 
+    // and add it into k
+    for (i = 1; i<n-1; i++) 
+    {  
+        j = j + 1; // Increasing j by 1 
+        k = k + j; // Add value of j into k and update k 
+    } 
+    cout<<k;
+} 
+// Driven Function 
+int main() 
+{ 
+    int n = 5; 
+    func(n); 
+    return 0; 
+} 
